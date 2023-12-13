7f7c373690b8aec2ba76da2a8ebab70df852e8b3
MrAnomaly`s <153665428+MrAnomalys@users.noreply.github.com>
2023-12-13 18:14:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-
+print ("Welcome to commison Calculator")
+Value1 = input("What is your name?: ")
+value2 = input("What is you revenue?: ") 
+print(f"So if your profit is {Value2} then your total profit is {Value2*13/100}
